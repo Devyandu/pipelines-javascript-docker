d9f80b21df04a763bb7cefeba47bfec20bef4153
unknown <devyandu@gmail.com>
2020-11-25 11:45:52 -0500

    more actions

--- app/server.js
+++ app/server.js
@@ -6,8 +6,42 @@ const PORT = 80;
 const HOST = '0.0.0.0';
 
 const app = express();
+app.use(express.json()); 
+
 app.get('/', (req, res) => {
   res.send('Hello Devyandu\n');
+});
+
+app.get('/profile', (req, res) => {
+  res.send({'name':'Anonymous'});
+});
+
+app.get('/plans', (req, res) => {
+  res.send(
+      [
+    {
+      name:'Super Saver 1000',
+      'planOptions':[
+        'High','Medium','Saver'
+      ]
+    },
+    {
+      name:'Super Saver 500',
+      'planOptions':[
+        'High','Medium','Low 500'
+      ]
+    },
+    {
+      name:'Choice 500',
+      'planOptions':[
+        'High','Medium','Choice'
+      ]
+    }
+  ]);
+});
+app.get('/nextAction', (req, res) => {
+  console.log(req.body);
+   res.send({'name':'plans'});
 });
 
 app.listen(PORT, HOST);
